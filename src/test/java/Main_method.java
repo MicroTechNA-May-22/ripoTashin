7589eb7fa22af270de2a749f77870a09defa80ce
Tkhanam <minatahmina2022@gmail.com>
2023-09-28 12:23:32 -0500

    second update

--- src/test/java/Main_method.java
+++ src/test/java/Main_method.java
@@ -2,7 +2,10 @@ public class Main_method {
     public static void main(String[] args) {
         System.out.println("This is my main method class I created  just now");
         System.out.println("Hello World");
-        System.out.println("This is my practice");
-        System.out.println("I am learning java");
+        System.out.println("This is Asma");
+        System.out.println("This is Fahmida");
+        System.out.println("This is Mina");
+        System.out.println("Rezwan bhai is helping");
+
     }
 }
